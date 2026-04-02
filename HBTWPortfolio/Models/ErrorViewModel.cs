220681acfe4bb19bcb4b032ed7c1c8af4ac63108
Nikhat Shazi <shazi.hbt@gmail.com>
2026-04-02 13:17:10 +0530

    Bootstrap slider portfolio for hbtworkspace.in

---
+++ HBTWPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HBTWPortfolio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
